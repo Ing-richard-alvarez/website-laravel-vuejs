8350ca0bd1339da8866be0e07d9c25061a047663
Richard Damian Alvarez Salazar <richardinho_1993@hotmail.com>
2024-03-13 00:06:21 -0500

    connecting with vin api

--- frontend-vue-api/src/components/Nav.vue
+++ frontend-vue-api/src/components/Nav.vue
@@ -1,6 +1,8 @@
 <script setup>
 import { ref } from 'vue';
+import { useAuthStore } from "../stores/auth";
 
+const authStore = useAuthStore();
 let open = ref(false);
 const MenuOpen = () => {
     open.value = !open.value;
@@ -9,29 +11,56 @@ const MenuOpen = () => {
 </script>
 
 <template>
-    <div class="bg-gray-900 text-gray-100 py-3.5 px-6 shadow md:flex justify-between items-center">
-        <div class="flex items-center cursor-pointer">
-            <span class="text-green-500 text-xl mr-1">
-                <i class="bi bi-messenger"></i>
-            </span>
-            <h1 class="text-xl">FullStack Test</h1>
-        </div>
+    <template v-if="!authStore.user">
+        <div class="bg-gray-900 text-gray-100 py-3.5 px-6 shadow md:flex justify-between items-center">
+            <div class="flex items-center cursor-pointer">
+                <span class="text-green-500 text-xl mr-1">
+                    <i class="bi bi-messenger"></i>
+                </span>
+                <h1 class="text-xl">FullStack Test</h1>
+            </div>
 
-        <span @click="MenuOpen()" class="absolute md:hidden ease-in right-6 top-1.5  cursor-pointer text-4xl">
-            <i :class="[open ? 'bi bi-x' : 'bi bi-filter-left']"></i>
-        </span>
-        <ul class="md:flex md:items-center md:px-0 px-6 md:pb-0 pb-10 md:static absolute
-        bg-gray-900 md:w-auto w-full top-14 duration-700 ease-in"
-        :class="[open ? 'left-0' : 'left-[-100%]']">
-            <li class="md:mx-4 md:my-0 my-6">
-                <router-link :to="{name:'Home'}" class="text-xl hover:text-green-500">Home</router-link>
-            </li>
-            <li class="md:mx-4 md:my-0 my-6">
-                <router-link :to="{name:'Login'}" class="text-xl hover:text-green-500">Login</router-link>
-            </li>
-            <li class="md:mx-4 md:my-0 my-6">
-                <router-link :to="{name:'Register'}" class="text-xl hover:text-green-500">Register</router-link>
-            </li>
-        </ul>
-    </div>
+            <span @click="MenuOpen()" class="absolute md:hidden ease-in right-6 top-1.5  cursor-pointer text-4xl">
+                <i :class="[open ? 'bi bi-x' : 'bi bi-filter-left']"></i>
+            </span>
+            <ul class="md:flex md:items-center md:px-0 px-6 md:pb-0 pb-10 md:static absolute
+            bg-gray-900 md:w-auto w-full top-14 duration-700 ease-in"
+            :class="[open ? 'left-0' : 'left-[-100%]']">
+                <li class="md:mx-4 md:my-0 my-6">
+                    <router-link :to="{name:'Home'}" class="text-xl hover:text-green-500">Home</router-link>
+                </li>
+                <li class="md:mx-4 md:my-0 my-6">
+                    <router-link :to="{name:'Login'}" class="text-xl hover:text-green-500">Login</router-link>
+                </li>
+                <li class="md:mx-4 md:my-0 my-6">
+                    <router-link :to="{name:'Register'}" class="text-xl hover:text-green-500">Register</router-link>
+                </li>
+            </ul>
+        </div>
+    </template>
+    <template v-else-if="authStore.user"> 
+        <div class="bg-gray-900 text-gray-100 py-3.5 px-6 shadow md:flex justify-between items-center">
+            <div class="flex items-center cursor-pointer">
+                <span class="text-green-500 text-xl mr-1">
+                    <i class="bi bi-messenger"></i>
+                </span>
+                <h1 class="text-xl">FullStack Test</h1>
+            </div>
+
+            <span @click="MenuOpen()" class="absolute md:hidden ease-in right-6 top-1.5  cursor-pointer text-4xl">
+                <i :class="[open ? 'bi bi-x' : 'bi bi-filter-left']"></i>
+            </span>
+            <ul class="md:flex md:items-center md:px-0 px-6 md:pb-0 pb-10 md:static absolute
+            bg-gray-900 md:w-auto w-full top-14 duration-700 ease-in"
+            :class="[open ? 'left-0' : 'left-[-100%]']">
+                <li class="md:mx-4 md:my-0 my-6">
+                    <router-link :to="{name:'Home'}" class="text-xl hover:text-green-500">Home</router-link>
+                </li>
+                <li class="md:mx-4 md:my-0 my-6">
+                    <button @click="authStore.handleLogout" class="text-xl hover:text-green-500">Logout</button>
+                </li>
+        
+            </ul>
+        </div>
+    </template>
 </template>
--- frontend-vue-api/src/stores/auth.js
+++ frontend-vue-api/src/stores/auth.js
@@ -7,6 +7,7 @@ import Swal from 'sweetalert2';
 export const useAuthStore = defineStore("auth", {
     state: () => ({
         authUser: null,
+        authInfoVIN: null,
         authErrors: [],
         authShowFormCreateUser: true,
         authShowFormSendCode: false,
@@ -15,6 +16,7 @@ export const useAuthStore = defineStore("auth", {
     }),
     getters: {
         user: (state) => state.authUser,
+        infoVIN: (state) => state.authInfoVIN,
         errors: (state) => state.authErrors,
         showFormCreateUser: (state) => state.authShowFormCreateUser,
         showFormSendCode: (state) => state.authShowFormSendCode,
@@ -193,6 +195,28 @@ export const useAuthStore = defineStore("auth", {
                 console.error(error);                
             }
             
+        },
+        async handleFindingVIN( form ) {
+            const vin = form.vin;
+
+            const options = {
+                method: 'GET',
+                url: 'https://vindecoder.p.rapidapi.com/decode_vin',
+                params: {
+                  vin: vin
+                },
+                headers: {
+                  'X-RapidAPI-Key': 'de2e861cfbmsh4044ee22d2fe00fp148010jsn92c0ef95db4d',
+                  'X-RapidAPI-Host': 'vindecoder.p.rapidapi.com'
+                }
+            };
+            try {
+                const response = await axios.request(options);
+                this.authInfoVIN = response.data;
+                console.log(response.data);
+            } catch (error) {
+                console.error(error);
+            }
         }
     }
 })
